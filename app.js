8e67709f7f934612655400381b7ba747b614d591
Salonee Gupta <saloneegupta269@gmail.com>
2021-11-26 16:23:57 +0530

    Add Procfile

--- app.js
+++ app.js
@@ -20,6 +20,11 @@ const classRoutes = require("./routes/class_.js");
 app.use("/", authRoutes);
 app.use("/classrooms", classRoutes);
 
-app.listen(3000, function () {
-  console.log("Server started on port 3000.");
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port, function () {
+  console.log("Server has started successfully");
 });
